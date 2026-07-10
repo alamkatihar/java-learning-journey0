02f7e3a2a710f18c2403966f0d08df507dbb9233
Shahbaj Alam <shahbajktr2001@gmail.com>
2026-07-10 22:05:30 +0530

    Day 1: Added Hello World program.

---
+++ JavaLearningJourney0/src/com/day01/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.day01;
+
+/**
+ * Day 01 - Hello World
+ *
+ * This program demonstrates the basic structure of a Java application and
+ * prints a simple message to the console.
+ *
+ * Author : Shahbaj Alam
+ */
+public class HelloWorld {
+
+	/**
+	 * Entry point of the Java application.
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World!");
+	}
+
+}
